d6c1f2be74de6692aee4bce4a69c1e50092eaee0
Silmaen <genteur.slayer@laposte.net>
2026-04-09 20:32:31 +0200

    Animated sprites

--- test/core_tests/version_test.cpp
+++ test/core_tests/version_test.cpp
@@ -6,6 +6,6 @@
 using namespace owl;
 
 TEST(Core_Version, base) {
-	EXPECT_STREQ("0.0.3", getVersionString().c_str());
-	EXPECT_EQ(0x00000300, getVersionCode());
+	EXPECT_STREQ("0.1.0", getVersionString().c_str());
+	EXPECT_EQ(0x00010000, getVersionCode());
 }
